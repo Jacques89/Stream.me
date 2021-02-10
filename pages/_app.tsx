9706cb192295a30fa63e590426e4c45fb59a1110
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-02-10 13:50:54 +0100

    structure for NextJs complete, linked pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
-import App from 'next/app'
-import React from 'react'
+import React, { useEffect } from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
 
 import { themeDark, themeLight } from 'lib/theme'
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Remove serverside injected CSS
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }, [])
+
   return(
     <ThemeProvider theme={false ? themeDark : themeLight}>
       <CssBaseline />
@@ -13,3 +20,4 @@ export default function MyApp({ Component, pageProps }) {
     </ThemeProvider>
   )
 }
+
