69bb9ffd79448ef3e6b7b78706e724f53e2c14b3
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-02-11 16:11:15 +0100

    created the apollo client for the app frontend

---
+++ lib/apollo.ts
@@ -0,0 +1,56 @@
+import { useMemo } from 'react'
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+  HttpLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
+
+function createApolloClient() {
+  // create an authentication link
+  const authLink = setContext((_, { headers }) => {
+    // Get the auth token from localStorage if it exists
+    // Token set within localStorage
+    const token = localStorage.getItem('token')
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    }
+  })
+  const httpLink = new HttpLink({
+    uri: 'http://localhost:8000/graphql',
+    credentials: 'include',
+  })
+
+  return new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  })
+}
+
+// Inititalise apollo client with context and initial state
+export function inititalizeApollo(initialState: any = null) {
+  const _apolloClient = apolloClient ?? createApolloClient()
+
+  // Initial apollo client state gets re-hydrated
+  if (initialState) {
+    _apolloClient.cache.restore(initialState)
+  }
+
+  // New Apollo client for SSR or SSG
+  if (typeof window === 'undefined') return _apolloClient
+
+  // Create apollo client once
+  if (!apolloClient) apolloClient = _apolloClient 
+  return _apolloClient
+}
+
+export function useApollo(initialState: any) {
+  const store = useMemo(() => inititalizeApollo(initialState), [initialState])
+  return store
+}
