f9d6c1e9de4995c00f23fb598ccd5c88726a8aec
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-02-11 18:52:20 +0100

    sign in, sign up and sign out functionality now working on the clientside

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { ApolloProvider } from '@apollo/client'
 import { useApollo } from 'lib/apollo'
 import { themeDark, themeLight } from 'lib/theme'
 import { AuthProvider } from 'lib/useAuth'
+import Header from 'components/Header'
 
 export default function MyApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
@@ -28,6 +29,7 @@ export default function MyApp({ Component, pageProps }) {
       <ThemeProvider theme={darkState ? themeDark : themeLight}>
         <CssBaseline />
         <AuthProvider>
+          <Header darkState={darkState} handleThemeChange={handleThemeChange} />
           <Component {...pageProps} />
         </AuthProvider>
       </ThemeProvider>
